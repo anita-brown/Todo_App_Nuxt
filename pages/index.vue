8377698d8e9e4030d7f156ec03874dcb187d34de
Anita <Efosa@Anita-Efosa.local>
2022-07-12 15:14:40 +0100

    chore: updated mutations ins store

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <Tutorial/>
+<main>
+  <h1>Task Board</h1>
+  <p>Create of list of tasks</p>
+  <div class="create-new">
+    <input type="text" v-model="newTask" placeholder="Add a new task">
+  </div>
+</main>
 </template>
 
 <script>
---
+++ store/index.js
@@ -0,0 +1,28 @@
+export const state = () => {
+  task: [
+    { }
+  ]
+}
+
+export const mutations = {
+  ADD_TASK(state, task) {
+    state.tasks = [task, ...state.tasks]
+    // state.tasks = [{ content: task, done: false}, ...state.tasks]
+  },
+  REMOVE_TASK(state, task) {
+    state.tasks.splice(state.tasks.indexOf(task), 1)
+  },
+  TOGGLE_TASK(state, task) {
+    task.done = !task.done;
+  }
+
+}
+export const actions = {
+
+}
+export const getters = {
+
+}
+export const modules = {
+
+}
